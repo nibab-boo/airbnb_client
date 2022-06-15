30333ec8e50b61247b92d0e75a5577bd18e7aeae
Babin Bohora <bohorababin1@gmail.com>
2022-06-15 18:05:08 +0900

    styling file and variable files created

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home container  mx-auto">
+    <div class="w-full px-3">
+      <input
+        class="placeholder-shown:border-black-500 border w-full rounded-lg p-2"
+        placeholder="you@example.com"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
