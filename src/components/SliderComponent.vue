222dc432c698fa768dd8324c90ac2b8ce66a7304
Babin Bohora <bohorababin1@gmail.com>
2022-06-18 19:14:33 +0900

    Slider in different component

---
+++ src/components/SliderComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <Splide :options="{ arrows: false,
+                        pagination: false,
+                        autoWidth: true,
+                        padding: { top: 10, bottom: 20 },
+                        direction: 'ltr',
+                        autoplay: false, }"
+      class="" aria-label="My Favorite Images">
+    <slot />
+  </Splide>
+</template>
+
+<script>
+import { Splide } from '@splidejs/vue-splide';
+
+export default {
+  name: 'SliderComponent',
+  components: {
+    Splide,
+  },
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
