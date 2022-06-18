972581eebe73b441026fecd7253172759f8f0365
Babin Bohora <bohorababin1@gmail.com>
2022-06-18 19:14:04 +0900

    SearchBar in different component

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="search mx-auto">
+    <div class="w-full flex drop-shadow-lg rounded-lg bg-white font-medium">
+      <span class="material-symbols-outlined my-auto">
+        chevron_left
+      </span>
+      <input
+        class="placeholder-shown:border-gray-0
+        focus:outline-none
+        p-2 w-full rounded-lg"
+        placeholder="you@example.com"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+};
+
+</script>
+
+<style lang="scss" scoped>
+</style>
