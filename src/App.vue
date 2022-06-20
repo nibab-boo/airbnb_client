242cc5fbea182381d3d80145224616ea1f7b7fa4
Babin Bohora <bohorababin1@gmail.com>
2022-06-20 12:15:30 +0900

    removed nav for develpment and added some css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
+  <div class="bg-slate-50 pt-5 p-6 container justify-center">
+    <!-- <nav>
+      <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    </nav> -->
+    <router-view></router-view>
+  </div>
 </template>
 
 <style lang="scss">
@@ -12,8 +14,7 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  // text-align: center;
 }
 
 nav {
